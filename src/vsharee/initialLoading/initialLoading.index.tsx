c455dd06e7ef0825f58b1d053e120ca1209431b6
Pooria Parhizkar <pouorix@gmail.com>
2025-07-13 18:42:41 +0330

    Add: pwa setup and initial loading pagE

--- src/vsharee/initialLoading/initialLoading.index.tsx
+++ src/vsharee/initialLoading/initialLoading.index.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
+import Circle from '../../assets/images/circle.png';
+import Logo from '../../assets/images/logo.png';
 import './initialLoading.style.scss';
 
 const InitialLoading: React.FC = () => {
     return (
-        <div className="initial-loading absolute top-0 left-0 z-50 flex h-screen w-screen items-center justify-center overflow-hidden">
-            <h1>Logo</h1>
+        <div className="initial-loading absolute top-0 left-0 z-50 flex h-screen w-screen items-center justify-center overflow-hidden bg-[linear-gradient(135deg,var(--color-primary),#3c0b10,var(--color-background))]">
+            <img className="animate-scale absolute z-10 opacity-25" src={Circle} alt="30pay" />
+            <img className="z-20 w-[100px]" src={Logo} alt="30pay" />
         </div>
     );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import checker from 'vite-plugin-checker';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
     plugins: [
         react(),
+        // VitePWA({
+        //     registerType: 'autoUpdate',
+        //     devOptions: {
+        //         enabled: true,
+        //     },
+        // }),
         tailwindcss(),
         checker({
             typescript: {
